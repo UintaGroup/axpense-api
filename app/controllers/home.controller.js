995db408934d5878fe07b3b5053b4782e3056ca4
Stone C. Lasley <stonelasley@gmail.com>
2017-08-01 22:28:27 -0600

    feat(style): Slap a theme in
    Added basic bootstrap theme to get team started

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -1,23 +1,13 @@
+let path = require('path');
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
-let Article = mongoose.model('Article');
 
 module.exports = app => {
   app.use('/', router);
 };
 
 router.get('/', (req, res, next) => {
-  Article.find((err, articles) => {
-
-    if (err) {
-      return next(err);
-    }
-
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
-
-  });
+  //TODO - move index into views
+  res.sendFile(path.join( __dirname + '/index.html'));
 });
